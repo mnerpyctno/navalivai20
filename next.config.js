24fb4ccc5d7080c594d3999eaa2c81f85f608bb9
mnerpyctno <artsemi.shyshkin@gmail.com>
2025-03-30 18:09:42 +0300

    Configure project for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['online.moysklad.ru'],
+  },
+  env: {
+    MOYSKLAD_TOKEN: process.env.MOYSKLAD_TOKEN,
+  },
   async rewrites() {
     return [
       {
