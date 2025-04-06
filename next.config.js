5f5a333d3aef96471c8bb49be9e1c48a096cb625
mnerpyctno <artsemi.shyshkin@gmail.com>
2025-04-06 20:39:42 +0300

    fix: удален rewrite для API, вызывающий бесконечный цикл

--- next.config.js
+++ next.config.js
@@ -15,14 +15,6 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://navalivai20.vercel.app/api/:path*',
-      }
-    ];
-  },
   async headers() {
     return [
       {
