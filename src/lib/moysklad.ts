d19009ee7ff798a6000d0f81ecee827713ac3314
mnerpyctno <artsemi.shyshkin@gmail.com>
2025-04-06 12:22:34 +0300

    fix: исправлены проблемы с TypeScript и добавлен класс MoySkladAPI

--- src/lib/moysklad.ts
+++ src/lib/moysklad.ts
@@ -1,1 +1,40 @@
- 
+import axios from 'axios';
+
+export class MoySkladAPI {
+  private baseURL: string;
+  private token: string;
+
+  constructor(token: string) {
+    this.baseURL = 'https://api.moysklad.ru/api/remap/1.2';
+    this.token = token;
+  }
+
+  private getHeaders() {
+    return {
+      'Authorization': `Bearer ${this.token}`,
+      'Content-Type': 'application/json'
+    };
+  }
+
+  async getProducts(params?: any) {
+    const response = await axios.get(`${this.baseURL}/entity/product`, {
+      headers: this.getHeaders(),
+      params
+    });
+    return response.data;
+  }
+
+  async getCategories() {
+    const response = await axios.get(`${this.baseURL}/entity/productfolder`, {
+      headers: this.getHeaders()
+    });
+    return response.data;
+  }
+
+  async getStock() {
+    const response = await axios.get(`${this.baseURL}/report/stock/bystore`, {
+      headers: this.getHeaders()
+    });
+    return response.data;
+  }
+} 
